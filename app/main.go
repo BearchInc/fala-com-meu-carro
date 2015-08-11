1bdf02765835629e46352535f4119c7e71856604
Heck/Ygor <heckfer@gmail.com>
2015-08-11 14:43:29 -0300

    [#00] Fix post creation. Add martini json renderer

--- app/main.go
+++ app/main.go
@@ -7,11 +7,14 @@ import (
 	"fmt"
 	"github.com/bearchinc/datastore-model"
 	"github.com/heckfer/ProjectCars/model"
+	"log"
+	"github.com/martini-contrib/render"
 )
 
 func init() {
 
 	router := martini.Classic()
+	router.Use(render.Renderer(render.Options{IndentJSON: true, }))
 
 	router.Post("/create_post", CreatePostHandler)
 	router.Get("/list_posts", ListPostsHandler)
@@ -20,9 +23,12 @@ func init() {
 }
 
 
-func CreatePostHandler(c martini.Context, req *http.Request) string {
+func CreatePostHandler(c martini.Context, req *http.Request, r render.Render) {
 	gae := appengine.NewContext(req)
 	namespace := appengine.ModuleName(gae)
+
+	log.Println(namespace)
+
 	context, err := appengine.Namespace(gae, namespace)
 	if err != nil {
 		panic(fmt.Sprintf("Could not create GAE context: %v", err))
@@ -30,28 +36,18 @@ func CreatePostHandler(c martini.Context, req *http.Request) string {
 
 	post := &model.Post{
 		CarPlate: "IWI-5585",
-		Message: "Lisardo perdeu outro cartão!",
+		Message: "I saw a credit card falling from this car! Thats crazy",
 	}
 
-	db.NewDatastore(context).Create(post)
-
-	return "OK"
+	err = db.NewDatastore(context).Create(post)
+	if err != nil {
+		log.Printf("Error: %+v", err)
+		r.JSON(500, "Error")
+	} else {
+		r.JSON(200, post)
+	}
 }
 
-func ListPostsHandler(c martini.Context, req *http.Request) string {
-	gae := appengine.NewContext(req)
-	namespace := appengine.ModuleName(gae)
-	context, err := appengine.Namespace(gae, namespace)
-	if err != nil {
-		panic(fmt.Sprintf("Could not create GAE context: %v", err))
-	}
-
-	post := &model.Post{
-		CarPlate: "IWI-5585",
-		Message: "Lisardo perdeu outro cartão!",
-	}
-
-	db.NewDatastore(context).Create(post)
-
-	return "OK"
+func ListPostsHandler(c martini.Context, req *http.Request, r render.Render) {
+	r.JSON(200, "ok")
 }
--- model/post.go
+++ model/post.go
@@ -3,7 +3,6 @@ import "github.com/bearchinc/datastore-model"
 
 type Post struct {
 	db.Model `db:"Post"`
-	Id       string `json:"id" db:"id"`
 	CarPlate string `json: car_plate`
 	Message  string `json: message`
 }
