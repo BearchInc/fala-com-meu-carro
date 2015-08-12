14dce66d3eac3d03e421560c11b7f10ef321cd8b
Heck/Ygor <heckfer@gmail.com>
2015-08-12 10:04:14 -0300

    [#00] Extract handlers to a beautiful new folder

--- app/main.go
+++ app/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"net/http"
-	"appengine"
-	"fmt"
-	"log"
-	"github.com/bearchinc/datastore-model"
 	"github.com/heckfer/ProjectCars/model"
 	"github.com/martini-contrib/render"
 	"github.com/martini-contrib/binding"
 	"github.com/go-martini/martini"
+	"github.com/heckfer/ProjectCars/handler"
 )
 
 func init() {
@@ -18,71 +15,10 @@ func init() {
 	m.Use(render.Renderer(render.Options{IndentJSON: true, }))
 
 	m.Group("/posts", func(r martini.Router) {
-		r.Post("/create", binding.Bind(model.Post{}), CreatePostHandler)
-		r.Get("/list", ListPostsHandler)
-		r.Get("/list/:plate", ListPostsByCarPlateHandler)
+		r.Post("/create", binding.Bind(model.Post{}), handler.CreatePostHandler)
+		r.Get("/list", handler.ListPostsHandler)
+		r.Get("/list/:plate", handler.ListPostsByCarPlateHandler)
 	})
 
 	http.Handle("/", m)
-}
-
-
-func CreatePostHandler(c martini.Context, req *http.Request, r render.Render, post model.Post) {
-
-	context := getAppengineContext(req)
-
-	err := db.NewDatastore(context).Create(&post)
-	if err != nil {
-		log.Printf("Error: %+v", err)
-		r.JSON(500, "Error")
-	} else {
-		r.JSON(200, post)
-	}
-}
-
-func ListPostsByCarPlateHandler(c martini.Context, req *http.Request, r render.Render, params martini.Params) {
-
-	context := getAppengineContext(req)
-
-	posts := model.Posts{}
-	carPlate := params["plate"]
-	err := db.NewDatastore(context).Query(db.From(new(model.Post)).Filter("CarPlate=", carPlate)).All(&posts)
-
-	if err != nil {
-		log.Printf("Error: %+v", err)
-		r.JSON(500, "Error")
-	} else {
-		r.JSON(200, posts)
-	}
-}
-
-
-func ListPostsHandler(c martini.Context, req *http.Request, r render.Render) {
-
-	context := getAppengineContext(req)
-
-	posts := model.Posts{}
-	err := db.NewDatastore(context).Query(db.From(new(model.Post))).All(&posts)
-
-	if err != nil {
-		log.Printf("Error: %+v", err)
-		r.JSON(500, "Error")
-	} else {
-		r.JSON(200, posts)
-	}
-}
-
-
-
-func getAppengineContext(request *http.Request) appengine.Context {
-	gae := appengine.NewContext(request)
-	namespace := appengine.ModuleName(gae)
-
-	context, err := appengine.Namespace(gae, namespace)
-
-	if err != nil {
-		panic(fmt.Sprintf("Could not create GAE context: %v", err))
-	}
-
-	return context
 }
