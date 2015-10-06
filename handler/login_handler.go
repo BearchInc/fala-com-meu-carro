23aee702852cd79419f935bbd3706c61ec5e24d1
Ygor <ygor.bruxel@gmail.com>
2015-10-06 13:33:12 -0300

    [#00] Create user

--- handler/login_handler.go
+++ handler/login_handler.go
@@ -1,10 +1,39 @@
 package handler
 
 import (
+	"log"
+
+	"net/http"
+
 	"github.com/drborges/appx"
 	"github.com/heckfer/fala-com-meu-carro/model"
 	"github.com/martini-contrib/render"
 )
 
-func LoginHandler(r render.Render, login model.User, appx *appx.Datastore) {
+func LoginHandler(r render.Render, user model.User, appx *appx.Datastore) {
+	response := model.Response{
+		ErrorCode: http.StatusOK,
+		Message:   []string{},
+		Data:      nil,
+	}
+
+	//isValid, validationErr := user.IsValid()
+
+	if !true {
+		response.ErrorCode = http.StatusBadRequest
+		//response.Message = validationErr
+	} else {
+		err := appx.Save(&user)
+
+		if err != nil {
+			log.Printf("Error: %v", err)
+			response.ErrorCode = http.StatusInternalServerError
+			response.Message = append(response.Message, err.Error())
+		} else {
+			response.Data = user
+		}
+	}
+
+	r.JSON(200, response)
+
 }
--- model/user.go
+++ model/user.go
@@ -4,7 +4,14 @@ import "github.com/drborges/appx"
 
 type User struct {
 	appx.Model
-	DisplayName string
-	Email       string
-	Password    string
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+}
+
+func (user *User) KeySpec() *appx.KeySpec {
+	return &appx.KeySpec{
+		Kind:       "User",
+		Incomplete: true,
+	}
 }
